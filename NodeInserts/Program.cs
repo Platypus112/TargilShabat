6a16f71e7091de3461022738c4843f271df39fca
User <User@LAB3-3>
2024-01-18 14:30:28 +0200

    lesson

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NodeClass;
 using NodeInserts.Models;
+using System.Runtime.CompilerServices;
 
 namespace NodeInserts
 {
@@ -14,7 +15,36 @@ namespace NodeInserts
             ShabatRecievers sh = shabatShalom.GetValue();
             string p1 = sh.GetParent1();
 
-
+        }
+        public static Node<ShabatRecievers> DeleteShabat(Node<ShabatRecievers> ShabatLst,string parent)
+        {
+            if (ShabatLst == null) return null;
+            if(ShabatLst.GetValue().GetParent1()==parent|| ShabatLst.GetValue().GetParent2() == parent)
+            {
+                Node<ShabatRecievers> otherHead = ShabatLst.GetNext();
+                ShabatLst.SetNext(null);
+                return otherHead;
+            }
+            Node<ShabatRecievers> head = ShabatLst;
+            while (ShabatLst!=null&&ShabatLst.HasNext())
+            {
+                Node<ShabatRecievers> next = ShabatLst.GetNext();
+                ShabatRecievers nextShabat = next.GetValue();
+                if (nextShabat.GetParent1()==parent|| nextShabat.GetParent2() == parent)
+                {
+                    ShabatLst.SetNext(next.GetNext());
+                    next.SetNext(null);
+                }
+                ShabatLst = ShabatLst.GetNext();
+            }
+            return head;
+        }
+        public static bool CheckShabat(Node<ShabatRecievers> ShabatLst,int day,int month,int year)
+        {
+            if (ShabatLst != null) return false;
+            ShabatRecievers that = ShabatLst.GetValue();
+            if (that.GetDay() == day && that.GetMonth() == month && that.GetYear() == year) return true;
+            return CheckShabat(ShabatLst.GetNext(), day, month, year);
         }
     }
 }
